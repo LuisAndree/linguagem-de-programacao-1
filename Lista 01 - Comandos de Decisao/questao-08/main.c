7f8f41c170c13ae50848e7205a4f9a5c7a1214b9
Luís André <andre2016999@gmail.com>
2024-07-27 18:50:44 -0300

    correção q8

--- Lista 01 - Comandos de Decisao/questao-08/main.c
+++ Lista 01 - Comandos de Decisao/questao-08/main.c
@@ -3,6 +3,28 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int km, litros;
+    int razaoKmPorLitros;
+
+    printf("Digite a quantidade de Km rodados: ");
+    scanf("%d", &km);
+
+    printf("Digite a quantidade de litros consumidos no percurso: ");
+    scanf("%d", &litros);
+
+    razaoKmPorLitros = km / litros;
+
+    if (razaoKmPorLitros < 8) {
+        printf("Seu carro fez %d Km/L \n", razaoKmPorLitros);
+        printf("Venda o carro!!");
+    } else if(razaoKmPorLitros >= 8 && razaoKmPorLitros <= 14){
+        printf("Seu carro fez %d Km/L \n", razaoKmPorLitros);
+       if (razaoKmPorLitros > 12 ) {
+        printf("Super Economico!!");
+       } else {
+        printf("Economico!!!");
+       }
+    }
+
     return 0;
 }
